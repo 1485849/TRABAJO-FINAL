cda97cc019a2e3be6b37e984e2e3a8e4af467c0b
1485849 <1485849@senati.pe>
2023-03-01 15:38:21 -0500

    direccion

--- TRABAJO FINAL.py
+++ TRABAJO FINAL.py
@@ -14,21 +14,41 @@ miframe.pack()
 obtenerDNI =stringVar()
 lDNI=Label(miframe, text="DNI: ")
 lDNI.grid(row=0, columna=0, sticky="e", pady=5, padx=5)
-tDNI=entry(miframe,textvariable=ontenerDni)
-tDNI.grid(row=0, column=1, pady=5, padx=5)
+tDni = Entry(miFrame,textvariable=obtenerDNI)
+tDni.grid(row=0, column=1, pady=5, padx=5)
+
 
          # etiqueta apellidos
 
 obtenerApellido=StringVar()
 lApellido = Label(miFrame, text="Apellido:")
-lApellido.grid(row=1, column=0, sticky='e', pady=5, padx=5)
+lApellido.grid(row=1, column=0, sticky="e", pady=5, padx=5)
 tApellido = Entry(miFrame,textvariable=obtenerApellido)
 tApellido.grid(row=1, column=1, pady=5, padx=5)
+
 
             # etiqueta nombre
 
 obtenerNombre=StringVar()
-lNombre = Label(miFrame, text='Nombre:')
-lNombre.grid(row=1, column=2, sticky='e', pady=5, padx=5)
+lNombre = Label(miFrame, text="Nombre:")
+lNombre.grid(row=1, column=2, sticky="e", pady=5, padx=5)
 tNombre = Entry(miFrame,textvariable=obtenerNombre)
-tNombre.grid(row=1, column=3, pady=5, padx=5)           
+tNombre.grid(row=1, column=3, pady=5, padx=5)  
+
+            # etiqueta direccion 
+
+obtenerDir=StringVar()
+lDireccion = Label(miFrame, text="Dirección:")
+lDireccion.grid(row=2, column=0, sticky="e", pady=5, padx=5)
+tDireccion = Entry(miFrame,textvariable=obtenerDir)
+tDireccion.grid(row=2, column=1, columnspan=3, sticky="we",pady=5, padx=5)
+
+
+             # etiqueta telefono
+
+
+obtenerTel=StringVar()
+lTel = Label(miFrame, text="Telefono:")
+lTel.grid(row=3, column=0, sticky="e", pady=5, padx=5)
+tTel = Entry(miFrame,textvariable=obtenerTel)
+tTel.grid(row=3, column=1,columnspan=3, sticky="we", pady=5, padx=5)
