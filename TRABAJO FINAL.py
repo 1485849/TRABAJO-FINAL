b8f95fe1db266d1ccbbb356ce775ae3b93bfcc36
1485849 <1485849@senati.pe>
2023-03-01 14:29:38 -0500

    actualizado

--- TRABAJO FINAL.py
+++ TRABAJO FINAL.py
@@ -1,2 +1,13 @@
 
 # Creación de Aplicaciones básicas en Python
+from tkinter import *
+
+root = tk()
+root.title("FERRETERIA EL TORNILLO FELIZ")
+
+miframe = frame(root)
+miframe.pack()
+
+obtenerDNI =stringVar()
+lDNI=Label(miframe, text="DNI: ")
+lDNI.grid(row=0, columna=0, sticky="e", pady=5, padx=5)
